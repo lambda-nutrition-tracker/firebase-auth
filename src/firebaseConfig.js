8b71b1263730f9111e9fb70f9b29a3f48ec96a69
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-10-06 16:28:31 -0700

    Basic Firebase auth. Config files will not work because Firebase project used in the example was deleted.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyCeODBX2kXQOshohO14Mu6zKNNaN0qdIiw",
+  authDomain: "fir-auth3-51fa1.firebaseapp.com",
+  databaseURL: "https://fir-auth3-51fa1.firebaseio.com",
+  projectId: "fir-auth3-51fa1",
+  storageBucket: "",
+  messagingSenderId: "921368027240",
+  appId: "1:921368027240:web:a258831ee8500367615bdd"
+};
+
+export default config;
